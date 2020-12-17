5a35a968a71ef6069a670c56d4f31237265a825a
Márk Ángyán <angyanmark97@gmail.com>
2020-12-17 21:38:37 +0100

    Movie lists

--- Filmster.Core/Services/TMDbService.cs
+++ Filmster.Core/Services/TMDbService.cs
@@ -9,6 +9,13 @@ namespace Filmster.Core.Services
     {
         private static readonly TMDbClient client = new TMDbClient("5e9bcb638329a15acf75c1b2d85ae67e");
 
+        public static readonly string SecureBaseUrl = "https://image.tmdb.org/t/p/";
+        public static readonly string BackdropSize = "w780";
+        public static readonly string LogoSize = "w300";
+        public static readonly string PosterSize = "w342";
+        public static readonly string ProfileSize = "w185";
+        public static readonly string StillSize = "w300";
+
         public static async Task<List<SearchMovie>> GetPopularMoviesAsync()
         {
             return (await client.GetMoviePopularListAsync()).Results;
---
+++ Filmster/Helpers/PosterPathToFullPathConverter.cs
@@ -0,0 +1,23 @@
+﻿using Filmster.Core.Services;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Filmster.Helpers
+{
+    public class PosterPathToFullPathConverter : IValueConverter
+    {
+        private readonly string SecureBaseUrl = TMDbService.SecureBaseUrl;
+        private readonly string PosterSize = TMDbService.PosterSize;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var path = value as string;
+            return $"{SecureBaseUrl}{PosterSize}{path}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
