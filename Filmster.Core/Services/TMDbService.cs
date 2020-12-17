e6af1685cee89a5441c69c5eb8d5f9307acf0eec
Márk Ángyán <angyanmark97@gmail.com>
2020-12-17 19:21:24 +0100

    GetPopularMovies

---
+++ Filmster.Core/Services/TMDbService.cs
@@ -0,0 +1,16 @@
+﻿using TMDbLib.Client;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+
+namespace Filmster.Core.Services
+{
+    public static class TMDbService
+    {
+        private static readonly TMDbClient client = new TMDbClient("5e9bcb638329a15acf75c1b2d85ae67e");
+
+        public static SearchContainer<SearchMovie> GetPopularMovies()
+        {
+            return client.GetMoviePopularListAsync().Result;
+        }
+    }
+}
--- Filmster/ViewModels/MoviesViewModel.cs
+++ Filmster/ViewModels/MoviesViewModel.cs
@@ -1,11 +1,17 @@
-﻿using Filmster.Helpers;
+﻿using Filmster.Core.Services;
+using Filmster.Helpers;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
 
 namespace Filmster.ViewModels
 {
     public class MoviesViewModel : Observable
     {
+        public SearchContainer<SearchMovie> PopularMovies { get; set; }
+
         public MoviesViewModel()
         {
+            PopularMovies = TMDbService.GetPopularMovies();
         }
     }
 }
