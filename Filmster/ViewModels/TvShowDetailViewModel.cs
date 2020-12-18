e85545acf4171a62d81345e1257735b1179d2972
Márk Ángyán <angyanmark97@gmail.com>
2020-12-18 18:17:58 +0100

    TV Show detail page

---
+++ Filmster/ViewModels/TvShowDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Filmster.Helpers;
+
+namespace Filmster.ViewModels
+{
+    public class TvShowDetailViewModel : Observable
+    {
+        public TvShowDetailViewModel()
+        {
+        }
+    }
+}
---
+++ Filmster/Views/TvShowDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Filmster.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Filmster.Views
+{
+    public sealed partial class TvShowDetailPage : Page
+    {
+        public TvShowDetailViewModel ViewModel { get; } = new TvShowDetailViewModel();
+
+        public TvShowDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
