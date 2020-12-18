dc53836c75242ff94581b227b190b689a6f9eca1
Márk Ángyán <angyanmark97@gmail.com>
2020-12-18 15:34:39 +0100

    Added Movie detail page

---
+++ Filmster/Views/MovieDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Filmster.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Filmster.Views
+{
+    public sealed partial class MovieDetailPage : Page
+    {
+        public MovieDetailViewModel ViewModel { get; } = new MovieDetailViewModel();
+
+        public MovieDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
