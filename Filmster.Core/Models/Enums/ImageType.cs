e8707d764e01cdcb35c7a71c1e64ce5cf6c51a1f
Márk Ángyán <angyanmark97@gmail.com>
2020-12-18 02:09:49 +0100

    ImagePathToFullPathConverter

---
+++ Filmster.Core/Models/Enums/ImageType.cs
@@ -0,0 +1,11 @@
+﻿namespace Filmster.Core.Models.Enums
+{
+    public enum ImageType
+    {
+        Backdrop = 1,
+        Logo = 2,
+        Poster = 3,
+        Profile = 4,
+        Still = 5,
+    }
+}
---
+++ Filmster/Helpers/ImagePathToFullPathConverter.cs
@@ -0,0 +1,53 @@
+﻿using Filmster.Core.Models.Enums;
+using Filmster.Core.Services;
+using System;
+using System.ComponentModel;
+using Windows.UI.Xaml.Data;
+
+namespace Filmster.Helpers
+{
+    public class ImagePathToFullPathConverter : IValueConverter
+    {
+        private readonly string SecureBaseUrl = TMDbService.SecureBaseUrl;
+        private readonly string BackdropSize = TMDbService.BackdropSize;
+        private readonly string LogoSize = TMDbService.LogoSize;
+        private readonly string PosterSize = TMDbService.PosterSize;
+        private readonly string ProfileSize = TMDbService.ProfileSize;
+        private readonly string StillSize = TMDbService.StillSize;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var path = value as string;
+            Enum.TryParse(parameter as string, out ImageType imageType);
+            string size;
+
+            switch (imageType)
+            {
+                case ImageType.Backdrop:
+                    size = BackdropSize;
+                    break;
+                case ImageType.Logo:
+                    size = LogoSize;
+                    break;
+                case ImageType.Poster:
+                    size = PosterSize;
+                    break;
+                case ImageType.Profile:
+                    size = ProfileSize;
+                    break;
+                case ImageType.Still:
+                    size = StillSize;
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException("parameter", (int)imageType, typeof(ImageType));
+            }
+
+            return $"{SecureBaseUrl}{size}{path}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
