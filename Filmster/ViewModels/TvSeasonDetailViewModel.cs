c14499711aff978821c6a32d01d3f16d5ec42727
Márk Ángyán <angyanmark97@gmail.com>
2020-12-23 15:22:25 +0100

    TV Season detail page

---
+++ Filmster/ViewModels/TvSeasonDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Filmster.Helpers;
+
+namespace Filmster.ViewModels
+{
+    public class TvSeasonDetailViewModel : Observable
+    {
+        public TvSeasonDetailViewModel()
+        {
+        }
+    }
+}
---
+++ Filmster/Views/TvSeasonDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Filmster.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Filmster.Views
+{
+    public sealed partial class TvSeasonDetailPage : Page
+    {
+        public TvSeasonDetailViewModel ViewModel { get; } = new TvSeasonDetailViewModel();
+
+        public TvSeasonDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
