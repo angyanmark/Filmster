27c8820d535c79a0ac80eed4d3170802fc157e1e
Márk Ángyán <angyanmark97@gmail.com>
2020-12-23 15:31:18 +0100

    TV season click navigation

--- Filmster/Helpers/MediaViewModelBase.cs
+++ Filmster/Helpers/MediaViewModelBase.cs
@@ -16,6 +16,7 @@ namespace Filmster.Helpers
         public ICommand TvShowClickedCommand;
         public ICommand SearchMovieClickedCommand;
         public ICommand SearchTvClickedCommand;
+        public ICommand SearchTvSeasonClickedCommand;
         public ICommand SearchPersonClickedCommand;
         public ICommand SearchBaseClickedCommand;
         public ICommand MovieCastClickedCommand;
@@ -36,6 +37,7 @@ namespace Filmster.Helpers
             TvShowClickedCommand = new RelayCommand<TvShow>(TvShowClicked);
             SearchMovieClickedCommand = new RelayCommand<SearchMovie>(SearchMovieClicked);
             SearchTvClickedCommand = new RelayCommand<SearchTv>(SearchTvClicked);
+            SearchTvSeasonClickedCommand = new RelayCommand<SearchTvSeason>(SearchTvSeasonClicked);
             SearchPersonClickedCommand = new RelayCommand<SearchPerson>(SearchPersonClicked);
             SearchBaseClickedCommand = new RelayCommand<SearchBase>(SearchBaseClicked);
             MovieCastClickedCommand = new RelayCommand<MovieCast>(MovieCastClicked);
@@ -60,6 +62,11 @@ namespace Filmster.Helpers
         private void SearchTvClicked(SearchTv searchTv)
         {
             NavigationService.Navigate(typeof(TvShowDetailPage), searchTv.Id);
+        }
+
+        private void SearchTvSeasonClicked(SearchTvSeason searchTvSeason)
+        {
+            NavigationService.Navigate(typeof(TvSeasonDetailPage), searchTvSeason.Id);
         }
 
         private void SearchPersonClicked(SearchPerson searchPerson)
