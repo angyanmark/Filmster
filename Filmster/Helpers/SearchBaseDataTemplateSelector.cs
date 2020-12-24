b9fadb2da025284a7793a57f61fc3eee06bcc7fd
Márk Ángyán <angyanmark97@gmail.com>
2020-12-24 15:22:56 +0100

    Search MovieItemTemplate

---
+++ Filmster/Helpers/SearchBaseDataTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Filmster.Helpers
+{
+    public class SearchBaseDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Movie { get; set; }
+        public DataTemplate Tv { get; set; }
+        public DataTemplate Person { get; set; }
+        public DataTemplate Unknown { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            var searchBase = item as SearchBase;
+            switch (searchBase.MediaType)
+            {
+                case MediaType.Movie:
+                    return Movie;
+                case MediaType.Tv:
+                    return Tv;
+                case MediaType.Person:
+                    return Person;
+                case MediaType.Unknown:
+                default:
+                    return Unknown;
+            }
+        }
+    }
+}
