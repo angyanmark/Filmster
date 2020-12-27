f19286478a50213aaaa78cefaa544cb9b3a5e3ee
Márk Ángyán <angyanmark97@gmail.com>
2020-12-27 11:40:33 +0100

    Discover page added

---
+++ Filmster/ViewModels/DiscoverViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Filmster.Helpers;
+
+namespace Filmster.ViewModels
+{
+    public class DiscoverViewModel : Observable
+    {
+        public DiscoverViewModel()
+        {
+        }
+    }
+}
---
+++ Filmster/Views/DiscoverPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Filmster.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Filmster.Views
+{
+    public sealed partial class DiscoverPage : Page
+    {
+        public DiscoverViewModel ViewModel { get; } = new DiscoverViewModel();
+
+        public DiscoverPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
