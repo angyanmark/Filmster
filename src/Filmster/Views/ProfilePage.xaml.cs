53631957fac79f2613a7cbdf9df028c7ecba0b44
Ángyán Márk <angyanmark97@gmail.com>
2021-09-09 14:51:41 +0200

    Profile page init

---
+++ src/Filmster/Views/ProfilePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Filmster.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Filmster.Views
+{
+    public sealed partial class ProfilePage : Page
+    {
+        public ProfileViewModel ViewModel { get; } = new ProfileViewModel();
+
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
