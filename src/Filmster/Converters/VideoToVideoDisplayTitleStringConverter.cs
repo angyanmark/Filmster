b848bb47b6a98841c3b8e167c6c88668d1a918d8
Ángyán Márk <angyanmark97@gmail.com>
2022-01-27 21:27:02 +0100

    tooltip on trailer text

---
+++ src/Filmster/Converters/VideoToVideoDisplayTitleStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using Filmster.Common.Helpers;
+using System;
+using TMDbLib.Objects.General;
+using Windows.UI.Xaml.Data;
+
+namespace Filmster.Converters
+{
+    public class VideoToVideoDisplayTitleStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var video = value as Video;
+            return $"{video.Name} ({video.Site}) [{video.Size}{"Video_PixelSize".GetLocalized()}]";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
